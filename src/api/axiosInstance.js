90493a94a37298f82ea1b535449a05e440f44dde
shekh-muqtasid <shekhmuqtasid@gmail.com>
2026-03-19 14:45:36 +0500

    feat: Initialize Axios instance with request and response interceptors for authentication and token refresh.

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -5,6 +5,7 @@ const baseURL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000'
 
 const axiosInstance = axios.create({
   baseURL,
+  withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
   }
